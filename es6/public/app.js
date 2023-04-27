9f4e12d508c159f20322c3fb19856bfb65d0c0de
Gagan <g.deepsingh1@gmail.com>
2023-04-27 12:15:07 -0600

    tag template changes

--- es6/public/app.js
+++ es6/public/app.js
@@ -1,6 +1,25 @@
 "use strict";
 
+var _templateObject;
+function _taggedTemplateLiteral(strings, raw) { if (!raw) { raw = strings.slice(0); } return Object.freeze(Object.defineProperties(strings, { raw: { value: Object.freeze(raw) } })); }
 var object = {
   name: "test"
 };
 console.log("The object name is ".concat(object.name));
+console.log("\uD83D\uDC4D");
+var truth = "JavaScript is a really fun language!";
+console.log(truth.includes('J')); //true
+console.log(truth.includes('J', 10)); //false
+
+console.log(truth.endsWith('language!')); //true
+console.log(truth.endsWith('JavaScript')); //false
+
+console.log("Man I really love 🚀, they are the best!".match(/(?:\uD83D\uDE80)/));
+
+// tag templates
+function myTaggedLiteral(strings, value1, value2) {
+  console.log("strings", strings);
+  console.log("value1", value1);
+  console.log("value2", value2);
+}
+myTaggedLiteral(_templateObject || (_templateObject = _taggedTemplateLiteral(["Hello ", " ", "!"])), object.name, 9);
--- es6/src/app.js
+++ es6/src/app.js
@@ -3,3 +3,25 @@ const object = {
 };
 
 console.log(`The object name is ${object.name}`); 
+
+console.log('\u{1F44D}');
+
+const truth = "JavaScript is a really fun language!";
+
+console.log(truth.includes('J')); //true
+console.log(truth.includes('J', 10)); //false
+
+console.log(truth.endsWith('language!')); //true
+console.log(truth.endsWith('JavaScript')); //false
+
+console.log("Man I really love 🚀, they are the best!".match(/🚀/u))
+
+
+// tag templates
+function myTaggedLiteral(strings, value1, value2) {
+    console.log("strings", strings);
+    console.log("value1", value1);
+    console.log("value2", value2);
+}
+
+myTaggedLiteral`Hello ${object.name} ${9}!`;
